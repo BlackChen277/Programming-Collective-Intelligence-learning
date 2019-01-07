e066045fb4fec002f767f6236b30a48966e14a8a
chen chen <blackchen277@gmail.com>
2019-01-07 15:24:57 +0800

    第二章

--- 03章-发现群组/clusters.py
+++ 03章-发现群组/clusters.py
@@ -1,4 +1,4 @@
-# from PIL import Image, ImageDraw
+from PIL import Image, ImageDraw
 
 
 def readfile(filename):
@@ -112,5 +112,222 @@ def printclust(clust, labels=None, n=0):
 
 blognames, words, data = readfile('blogdata.txt')
 
-clust = hcluster(data)
-printclust(clust, blognames)
+# clust = hcluster(data)
+# printclust(clust, blognames)
+
+
+
+def getheight(clust):
+  # Is this an endpoint? Then the height is just 1
+  if clust.left==None and clust.right==None: return 1
+
+  # Otherwise the height is the same of the heights of
+  # each branch
+  return getheight(clust.left)+getheight(clust.right)
+
+def getdepth(clust):
+  # The distance of an endpoint is 0.0
+  if clust.left==None and clust.right==None: return 0
+
+  # The distance of a branch is the greater of its two sides
+  # plus its own distance
+  return max(getdepth(clust.left),getdepth(clust.right))+clust.distance
+
+
+
+
+def drawdendrogram(clust,labels,jpeg='clusters.jpg'):
+  # height and width
+  h=getheight(clust)*20
+  w=1200
+  depth=getdepth(clust)
+
+  # width is fixed, so scale distances accordingly
+  scaling=float(w-150)/depth
+
+  # Create a new image with a white background
+  img=Image.new('RGB',(w,h),(255,255,255))
+  draw=ImageDraw.Draw(img)
+
+  draw.line((0,h/2,10,h/2),fill=(255,0,0))
+
+  # Draw the first node
+  drawnode(draw,clust,10,(h/2),scaling,labels)
+  img.save(jpeg,'JPEG')
+
+
+def drawnode(draw, clust, x, y, scaling, labels):
+    if clust.id < 0:
+        h1 = getheight(clust.left) * 20
+        h2 = getheight(clust.right) * 20
+        top = y - (h1 + h2) / 2
+        bottom = y + (h1 + h2) / 2
+        # Line length
+        ll = clust.distance * scaling
+        # Vertical line from this cluster to children
+        draw.line((x, top + h1 / 2, x, bottom - h2 / 2), fill=(255, 0, 0))
+
+        # Horizontal line to left item
+        draw.line((x, top + h1 / 2, x + ll, top + h1 / 2), fill=(255, 0, 0))
+
+        # Horizontal line to right item
+        draw.line((x, bottom - h2 / 2, x + ll, bottom - h2 / 2), fill=(255, 0, 0))
+
+        # Call the function to draw the left and right nodes
+        drawnode(draw, clust.left, x + ll, top + h1 / 2, scaling, labels)
+        drawnode(draw, clust.right, x + ll, bottom - h2 / 2, scaling, labels)
+    else:
+        # If this is an endpoint, draw the item label
+        draw.text((x + 5, y - 7), labels[clust.id], (0, 0, 0))
+
+
+# drawdendrogram(clust,blognames,jpeg="blogclust.jpg")
+
+
+def rotatematrix(data):
+  newdata=[]
+  for i in range(len(data[0])):
+    newrow=[data[j][i] for j in range(len(data))]
+    newdata.append(newrow)
+  return newdata
+
+#
+# rdata = rotatematrix(data)
+# hcluster(rdata)
+# wordclust = hcluster(rdata)
+# drawdendrogram(wordclust,words,"wordclust.jpg")
+
+import random
+
+
+def kcluster(rows, distance=pearson, k=4):
+    # Determine the minimum and maximum values for each point
+    ranges = [(min([row[i] for row in rows]), max([row[i] for row in rows]))
+              for i in range(len(rows[0]))]
+
+    # Create k randomly placed centroids
+    clusters = [[random.random() * (ranges[i][1] - ranges[i][0]) + ranges[i][0]
+                 for i in range(len(rows[0]))] for j in range(k)]
+
+    lastmatches = None
+    for t in range(100):
+        print('Iteration %d' % t)
+        bestmatches = [[] for i in range(k)]
+
+        # Find which centroid is the closest for each row
+        for j in range(len(rows)):
+            row = rows[j]
+            bestmatch = 0
+            for i in range(k):
+                d = distance(clusters[i], row)
+                if d < distance(clusters[bestmatch], row): bestmatch = i
+            bestmatches[bestmatch].append(j)
+
+        # If the results are the same as last time, this is complete
+        if bestmatches == lastmatches: break
+        lastmatches = bestmatches
+
+        # Move the centroids to the average of their members
+        for i in range(k):
+            avgs = [0.0] * len(rows[0])
+            if len(bestmatches[i]) > 0:
+                for rowid in bestmatches[i]:
+                    for m in range(len(rows[rowid])):
+                        avgs[m] += rows[rowid][m]
+                for j in range(len(avgs)):
+                    avgs[j] /= len(bestmatches[i])
+                clusters[i] = avgs
+
+    return bestmatches
+
+
+kclust = kcluster(data,k=10)
+
+
+# for r in kclust:
+#     for x in r:
+#         print(blognames[x])
+
+
+def tanamoto(v1, v2):
+    c1, c2, shr = 0, 0, 0
+
+    for i in range(len(v1)):
+        if v1[i] != 0: c1 += 1  # in v1
+        if v2[i] != 0: c2 += 1  # in v2
+        if v1[i] != 0 and v2[i] != 0: shr += 1  # in both
+
+    return 1.0 - (float(shr) / (c1 + c2 - shr))
+
+
+wants,people,data=readfile('zebo.txt')
+
+clust = hcluster(data,distance=tanamoto)
+
+drawdendrogram(clust,wants,'zebo.jpg')
+
+
+def scaledown(data, distance=pearson, rate=0.01):
+    n = len(data)
+
+    # The real distances between every pair of items
+    realdist = [[distance(data[i], data[j]) for j in range(n)]
+                for i in range(0, n)]
+
+    # Randomly initialize the starting points of the locations in 2D
+    loc = [[random.random(), random.random()] for i in range(n)]
+    fakedist = [[0.0 for j in range(n)] for i in range(n)]
+
+    lasterror = None
+    for m in range(0, 1000):
+        # Find projected distances
+        for i in range(n):
+            for j in range(n):
+                fakedist[i][j] = sqrt(sum([pow(loc[i][x] - loc[j][x], 2)
+                                           for x in range(len(loc[i]))]))
+
+        # Move points
+        grad = [[0.0, 0.0] for i in range(n)]
+
+        totalerror = 0
+        for k in range(n):
+            for j in range(n):
+                if j == k: continue
+                # The error is percent difference between the distances
+                errorterm = (fakedist[j][k] - realdist[j][k]) / realdist[j][k]
+
+                # Each point needs to be moved away from or towards the other
+                # point in proportion to how much error it has
+                grad[k][0] += ((loc[k][0] - loc[j][0]) / fakedist[j][k]) * errorterm
+                grad[k][1] += ((loc[k][1] - loc[j][1]) / fakedist[j][k]) * errorterm
+
+                # Keep track of the total error
+                totalerror += abs(errorterm)
+        print
+        totalerror
+
+        # If the answer got worse by moving the points, we are done
+        if lasterror and lasterror < totalerror: break
+        lasterror = totalerror
+
+        # Move each of the points by the learning rate times the gradient
+        for k in range(n):
+            loc[k][0] -= rate * grad[k][0]
+            loc[k][1] -= rate * grad[k][1]
+
+    return loc
+
+def draw2d(data,labels,jpeg='mds2d.jpg'):
+  img=Image.new('RGB',(2000,2000),(255,255,255))
+  draw=ImageDraw.Draw(img)
+  for i in range(len(data)):
+    x=(data[i][0]+0.5)*1000
+    y=(data[i][1]+0.5)*1000
+    draw.text((x,y),labels[i],(0,0,0))
+  img.save(jpeg,'JPEG')
+
+
+blognames,words,data = readfile('blogdata.txt')
+
+coords = scaledown(data)
+draw2d(coords,blognames,'blogs2d.jpg')
---
+++ 03章-发现群组/notes.md
@@ -0,0 +1,3 @@
+- [深入理解K-Means算法](https://blog.csdn.net/taoyanqi8932/article/details/53727841)
+
+- [数据挖掘十大算法](https://wizardforcel.gitbooks.io/dm-algo-top10/content/index.html)
